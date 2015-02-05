acc0b6eaccf2901415a8e110770abcc060a6223b
Tempurturtul <tempurturtul@gmail.com>
2015-02-04 19:20:14 -0500

    Two new completed tasks. (C#)

---
+++ FibonacciSequence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+
+public class FibonacciSequence
+{
+	private static void Main()
+	{
+        int numOfCases = int.Parse(Console.ReadLine());
+        string answer = "";
+
+        for (int i = 0; i < numOfCases; i++)
+        {
+            string num = Console.ReadLine();
+            string a = "0";
+            string b = "1";
+            int count = 0;
+
+            while (a != num)
+            {
+                string result = (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString();
+                a = b;
+                b = result;
+                count++;
+            }
+
+            answer += count.ToString() + " ";
+        }
+
+        Console.WriteLine(answer);
+	}
+}
---
+++ Triangles.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Triangles
+{
+	private static void Main()
+	{
+        int numOfCases = int.Parse(Console.ReadLine());
+        string answer = "";
+
+        for (int i = 0; i < numOfCases; i++)
+        {
+            string[] sides = Console.ReadLine().Split(' ');
+            int sideA = int.Parse(sides[0]);
+            int sideB = int.Parse(sides[1]);
+            int sideC = int.Parse(sides[2]);
+            string result = "0 ";
+
+            if (sideA > sideB && sideA > sideC)
+            {
+                if (sideB + sideC >= sideA)
+                {
+                    result = "1 ";
+                }
+            }
+            else if (sideB > sideC)
+            {
+                if (sideA + sideC >= sideB)
+                {
+                    result = "1 ";
+                }
+            }
+            else
+            {
+                if (sideA + sideB >= sideC)
+                {
+                    result = "1 ";
+                }
+            }
+
+            answer += result;
+        }
+
+        Console.WriteLine(answer);
+	}
+}
